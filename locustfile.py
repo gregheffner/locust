6d991dc6c388bea2ddfaa3a09a58adb6266d712b
gregheffner <gregheffner@comcast.net>
2024-11-17 11:16:03 -0500

    add notes

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,19 @@
-from locust import HttpUser, task, between
+"""
+This script defines a Locust performance test user for load testing a web application.
+
+Classes:
+    BasicTestUser (HttpUser): A Locust user class that simulates a user performing tasks on a web application.
+
+Tasks:
+    load_homepage: Simulates a user loading the homepage.
+    load_about_page: Simulates a user loading the about page.
+
+Attributes:
+    wait_time (between): The wait time between tasks, set to a random value between 1 and 5 seconds.
+"""
+
+from locust import HttpUser, between, task
+
 
 class BasicTestUser(HttpUser):
     wait_time = between(1, 5)  # Wait time between tasks
--- start_workers.sh
+++ start_workers.sh
@@ -1,3 +1,11 @@
+# This script starts multiple Locust worker processes to distribute load testing.
+# 
+# Variables:
+#   NUM_WORKERS: The number of worker processes to start (default is 8).
+#
+# The script uses a for loop to start the specified number of worker processes.
+# Each worker process is started in the background using the '&' operator.
+# The workers connect to the Locust master running on localhost (127.0.0.1).
 #!/bin/bash
 
 NUM_WORKERS=8
